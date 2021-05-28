b8bb909f0a144ab4c6ef8bf89b1067252ba90f19
root <root@bastion.example.com>
2021-05-28 11:30:16 -0400

    add index changes

--- basic-nginx/promote.sh
+++ basic-nginx/promote.sh
@@ -1,3 +1,9 @@
+git checkout master
+echo "test" >> index.html
+git add -A
+git commit -m "add index changes"
+git push origin master
+git checkout nerw
 cp index.html build/index.html
 cp nginx.conf build/nginx.conf
 oc start-build basic-nginx --from-dir="." -F -w -n basic-nginx-build
