6eb059fb0190af10e435144890972c51d91fbce8
root <root@bastion.example.com>
2021-05-28 11:01:38 -0400

    Added some changes

--- basic-nginx/promote.sh
+++ basic-nginx/promote.sh
@@ -1,4 +1,6 @@
-oc start-build basic-nginx -w -n basic-nginx-build
+cp index.html build/index.html
+cp nginx.conf build/nginx.conf
+oc start-build basic-nginx --from-dir="." -F -w -n basic-nginx-build
 oc tag basic-nginx:latest basic-nginx:basic-nginx-build-dev -n basic-nginx-build
 oc tag basic-nginx:latest basic-nginx:basic-nginx-build-stage -n basic-nginx-build
 oc tag basic-nginx:latest basic-nginx:basic-nginx-build-prod -n basic-nginx-build
